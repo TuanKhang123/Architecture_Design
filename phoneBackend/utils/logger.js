e7545136f63adde4f63cce00a1edc3b303fa3fdb
TuanKhang123 <20521437@gm.uit.edu.vn>
2024-05-20 14:59:57 +0700

    'fix-logging-winston'

--- phoneBackend/utils/logger.js
+++ phoneBackend/utils/logger.js
@@ -2,7 +2,12 @@ var winston = require('winston');
 
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), // Add timestamp
+        winston.format.printf(({ timestamp, level, message, meta }) => {
+            return `${timestamp} [${level.toUpperCase()}]: ${message} ${meta ? JSON.stringify(meta) : ''}`;
+        })
+    ),
     transports: [
         new winston.transports.File({ filename: 'error.log', level: 'error' }),
         new winston.transports.File({ filename: 'combined.log' }),
